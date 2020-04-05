588427991f5d87b260afe32c656db4a54d224382
muggle <isocket@outlook.com>
2020-04-05 09:47:27 +0800

    web json处理器

---
+++ src/main/java/com/muggle/poseidon/handler/WebResultHandler.java
@@ -0,0 +1,111 @@
+package com.muggle.poseidon.handler;
+
+import com.muggle.poseidon.base.ResultBean;
+import com.muggle.poseidon.base.exception.BasePoseidonCheckException;
+import com.muggle.poseidon.base.exception.BasePoseidonException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class WebResultHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(WebResultHandler.class);
+
+
+    /**
+     * 自定义异常
+     *
+     * @param e
+     * @param req
+     * @return
+     */
+    @ExceptionHandler(value = {BasePoseidonException.class})
+    public ResultBean poseidonExceptionHandler(BasePoseidonException e, HttpServletRequest req) {
+        log.error("业务异常",e);
+        ResultBean error = ResultBean.error(e.getMessage());
+        return error;
+    }
+
+    /**
+     * 参数未通过校验
+     *
+     * @param e
+     * @param req
+     * @return
+     */
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResultBean MethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest req) {
+        log.error("参数未通过校验",e);
+        ResultBean error = ResultBean.error(e.getMessage());
+        return error;
+    }
+
+    /**
+     * 错误的请求方式
+     *
+     * @param e
+     * @param req
+     * @return
+     */
+    @ExceptionHandler(value = {HttpRequestMethodNotSupportedException.class})
+    public ResultBean notsupported(Exception e, HttpServletRequest req) {
+        log.error("错误的请求方式",e);
+        ResultBean error = ResultBean.error("错误的请求方式");
+        return error;
+    }
+
+    /**
+     * 请求路径不存在
+     *
+     * @param e
+     * @param req
+     * @return
+     */
+    @ExceptionHandler(value = {NoHandlerFoundException.class})
+    public ResultBean notFoundUrl(Exception e, HttpServletRequest req) {
+        log.error("请求路径不存在",e);
+        ResultBean error = ResultBean.error("请求路径不存在");
+        return error;
+    }
+
+
+    /**
+     * 自定义异常
+     *
+     * @param e
+     * @param req
+     * @return
+     */
+    @ExceptionHandler(value = {BasePoseidonCheckException.class})
+    public ResultBean checked(Exception e, HttpServletRequest req) {
+        log.error("自定义异常",e);
+        ResultBean error = ResultBean.error(e.getMessage());
+        return error;
+    }
+
+    /**
+     * 未知异常，需要通知到管理员,对于线上未知的异常，我们应该严肃处理：先将消息传给MQ中心(该平台未实现) 然后日志写库
+      * @param e
+     * @param req
+     * @return
+     */
+    @ExceptionHandler(value = {Exception.class})
+    public ResultBean exceptionHandler(Exception e, HttpServletRequest req) {
+        try {
+            log.error("系统异常：" + req.getMethod() + req.getRequestURI()+" user: "+ SecurityContextHolder.getContext().getAuthentication().getPrincipal(), e);
+            // todo
+            return ResultBean.error("系统异常");
+        }catch (Exception err){
+            log.error("紧急！！！ 严重的异常",err);
+            return ResultBean.error("系统发生严重的错误");
+        }
+    }
+}
---
+++ src/main/java/com/muggle/poseidon/handler/WebUrlHandler.java
@@ -0,0 +1,53 @@
+package com.muggle.poseidon.handler;
+
+import com.muggle.poseidon.base.ResultBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @program: poseidon-cloud-core
+ * @description:
+ * @author: muggle
+ * @create: 2020-03-12 09:54
+ */
+@RestController
+public class WebUrlHandler implements ErrorController {
+
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    /**
+     * logger
+     */
+    private static final Logger log = LoggerFactory.getLogger(WebUrlHandler.class);
+
+    @RequestMapping(value = "/public/notfound", produces = "application/json;charset=UTF-8")
+    public ResultBean notfund(HttpServletRequest request) {
+        log.info("》》》》》》》》》》》》》》》》》 客户端访问了错误的页面");
+        return ResultBean.error("找不到页面", 404);
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+
+    @RequestMapping("/")
+    public ResultBean getMessage() {
+        return ResultBean.successData(appName);
+    }
+
+
+    @RequestMapping(value = "/error", produces = "application/json;charset=UTF-8")
+    public ResultBean error(HttpServletRequest request) {
+        return ResultBean.error("请求未响应，请稍后再试");
+    }
+}
