3187f0ac9f1d3aef2146c7369e936d32236aab6f
muggle <isocket@outlook.com>
2020-05-26 11:21:22 +0800

    查询bean的设计

--- src/main/java/com/muggle/poseidon/util/UserInfoUtils.java
+++ src/main/java/com/muggle/poseidon/util/UserInfoUtils.java
@@ -3,8 +3,11 @@ package com.muggle.poseidon.util;
 import com.muggle.poseidon.base.exception.BasePoseidonCheckException;
 import com.muggle.poseidon.base.exception.SimplePoseidonCheckException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 /**
  * @program: poseidon-cloud-starter
@@ -18,7 +21,42 @@ public class UserInfoUtils {
     public static UserDetails getUserInfo() throws BasePoseidonCheckException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication==null||authentication.getDetails()==null){
-            return null;
+            return new UserDetails() {
+                @Override
+                public Collection<? extends GrantedAuthority> getAuthorities() {
+                    return null;
+                }
+
+                @Override
+                public String getPassword() {
+                    return null;
+                }
+
+                @Override
+                public String getUsername() {
+                    return "无用户信息";
+                }
+
+                @Override
+                public boolean isAccountNonExpired() {
+                    return false;
+                }
+
+                @Override
+                public boolean isAccountNonLocked() {
+                    return false;
+                }
+
+                @Override
+                public boolean isCredentialsNonExpired() {
+                    return false;
+                }
+
+                @Override
+                public boolean isEnabled() {
+                    return false;
+                }
+            };
         }
         if (!(authentication.getDetails() instanceof UserDetails )){
             throw new SimplePoseidonCheckException("用户登陆信息异常");
