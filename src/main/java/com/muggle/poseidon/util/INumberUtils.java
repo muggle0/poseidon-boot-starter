ce9eab022ab6b5538437673b25ec1770dedd0093
muggle <isocket@outlook.com>
2020-06-05 14:39:39 +0800

    完善

---
+++ src/main/java/com/muggle/poseidon/util/INumberUtils.java
@@ -0,0 +1,11 @@
+package com.muggle.poseidon.util;
+
+import org.springframework.util.NumberUtils;
+
+/**
+ * @Description:
+ * @Author: muggle
+ * @Date: 2020/6/5
+ **/
+public class INumberUtils extends NumberUtils {
+}
---
+++ src/main/java/com/muggle/poseidon/util/IStringUtils.java
@@ -0,0 +1,11 @@
+package com.muggle.poseidon.util;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * @Description:
+ * @Author: muggle
+ * @Date: 2020/6/5
+ **/
+public class IStringUtils extends StringUtils {
+}
